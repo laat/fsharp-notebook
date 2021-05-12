9c925d1e543b75df9a1b18a9381df9311f50f023
Sigurd Fosseng <sigurd@fosseng.net>
2021-05-12 22:10:41 +0200

    .

---
+++ ddd.fsx
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S dotnet fsi --quiet
+// https://gist.github.com/swlaschin/54cfff886669ccab895a
+
+module Domain =
+  type AtLeastOne =
+    private
+      {
+        A: int option
+        B: int option
+        C: int option
+      }
+
+  module AtLeastOne =
+
+    let create aOpt bOpt cOpt =
+      match aOpt, bOpt, cOpt with
+      | (Some a, _, _) -> Some <| { A = aOpt; B = bOpt; C = cOpt }
+      | (_, Some b, _) -> Some <| { A = aOpt; B = bOpt; C = cOpt }
+      | (_, _, Some c) -> Some <| { A = aOpt; B = bOpt; C = cOpt }
+      | _ -> None
+
+    let createWhenAExists a bOpt cOpt = { A = Some a; B = bOpt; C = cOpt }
+    let createWhenBExists aOpt b cOpt = { A = aOpt; B = Some b; C = cOpt }
+    let createWhenCExists aOpt bOpt c = { A = aOpt; B = bOpt; C = Some c }
+
+    let value atLeastOne =
+      let a = atLeastOne.A
+      let b = atLeastOne.B
+      let c = atLeastOne.C
+      {| A = a; B = b; C = c |}
+
+open Domain
+
+let theOne = AtLeastOne.createWhenAExists 1 None None
+let theOneValue = theOne |> AtLeastOne.value
+theOneValue.B // value on the anonymous record :D
