d06967ac854881dd5c65358ec511f95bae48dd60
Sigurd Fosseng <sigurd@fosseng.net>
2021-05-15 19:51:54 +0200

    .

---
+++ src/bfs.fsx
@@ -0,0 +1,72 @@
+#!/usr/bin/env -S dotnet fsi --quiet
+open System.Collections.Generic
+
+type Node =
+  { Value: int
+    Left: Node option
+    Right: Node option }
+
+let tree =
+  { Value = 9
+    Left =
+      Some
+        { Value = 3
+          Left = Some { Value = 1; Right = None; Left = None }
+          Right = Some { Value = 4; Right = None; Left = None } }
+    Right = Some { Value = 5; Left = None; Right = None } }
+
+let rec dfs (res: Node list) (stack: Stack<Node>) =
+  if stack.Count = 0 then
+    res
+  else
+    let node = stack.Pop()
+
+    if node.Right.IsSome then
+      stack.Push(node.Right.Value)
+
+    if node.Left.IsSome then
+      stack.Push(node.Left.Value)
+
+    dfs (node :: res) stack
+
+let rec bfs (queue: Queue<Node>) res =
+  if queue.Count = 0 then
+    res
+  else
+    let node = queue.Dequeue()
+
+    if node.Left.IsSome then
+      queue.Enqueue(node.Left.Value)
+
+    if node.Right.IsSome then
+      queue.Enqueue(node.Right.Value)
+
+    bfs queue (node :: res)
+
+let rec swap computed (nodes: Node list) =
+  match nodes with
+  | [] -> computed
+  | head :: tail ->
+      swap
+        (Map.add
+          (Some head)
+          { Value = head.Value
+            Left = Map.tryFind head.Right computed
+            Right = Map.tryFind head.Left computed }
+          computed)
+        tail
+
+let printDfs prefix node =
+  Stack([ node ])
+  |> dfs []
+  |> List.rev
+  |> List.map (fun x -> x.Value)
+  |> printfn "%s %A" prefix
+
+
+tree |> printDfs "original"
+
+bfs (Queue([ tree ])) []
+|> swap Map.empty
+|> Map.find (Some tree)
+|> printDfs "inverted"
