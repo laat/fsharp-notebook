be579bf9548e3f983027bd5f68b7d23bc7fb1b2d
Sigurd Fosseng <sigurd@fosseng.net>
2021-05-17 00:15:43 +0200

    .

--- src/invert-tree-cont2.fsx
+++ src/invert-tree-cont2.fsx
@@ -4,19 +4,19 @@ type Node =
     Left: Node option
     Right: Node option }
 
-// let tree =
-//   { Value = 9
-//     Left =
-//       Some
-//         { Value = 3
-//           Left = Some { Value = 1; Right = None; Left = None }
-//           Right = Some { Value = 4; Right = None; Left = None } }
-//     Right = Some { Value = 5; Left = None; Right = None } }
-
 let tree =
-  [ 1 .. 305000 ]
-  |> List.fold (fun (root: Node option) i -> Some { Value = i; Left = None; Right = root }) None
-  |> Option.get
+  { Value = 9
+    Left =
+      Some
+        { Value = 3
+          Left = Some { Value = 1; Right = None; Left = None }
+          Right = Some { Value = 4; Right = None; Left = None } }
+    Right = Some { Value = 5; Left = None; Right = None } }
+
+// let tree =
+//   [ 1 .. 305000 ]
+//   |> List.fold (fun (root: Node option) i -> Some { Value = i; Left = None; Right = root }) None
+//   |> Option.get
 
 let printDfs prefix node =
   let rec dfs result (stack: Node list) =
@@ -36,9 +36,7 @@ let printDfs prefix node =
 
 type ContinuationBuilder() =
   member this.Return(x) = (fun k -> k x)
-  member this.ReturnFrom(x) = x
   member this.Bind(m, f) = (fun k -> m (fun a -> f a k))
-  member this.Delay(f) = (fun k -> f () k)
 
 let cps = ContinuationBuilder()
 
