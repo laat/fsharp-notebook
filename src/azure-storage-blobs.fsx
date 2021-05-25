a50185249163953c699e05fa98bce8adddced6c9
Sigurd Fosseng <sigurd@fosseng.net>
2021-05-25 20:56:30 +0200

    .

--- src/azure-storage-blobs.fsx
+++ src/azure-storage-blobs.fsx
@@ -1,46 +1,114 @@
 #!/usr/bin/env -S dotnet fsi --quiet
-#r "nuget: Azure.Storage.Blobs, 12.8.4"
-#load "helpers.fsx"
+#r "nuget: Azure.Storage.Blobs, 12.8.4"#load "helpers.fsx"
+namespace Azure.Storage.Blobs
 
-open Helpers
-open Azure
-open Azure.Storage.Blobs
-open Azure.Storage.Blobs.Models
+[<AutoOpen>]
+module BlobClient =
+    open System
+    open System.IO
+    open Azure
+    open Azure.Storage.Blobs.Models
 
+    type BlobClient with
+        member this.AsyncDownload(?range: HttpRange, ?conditions: BlobRequestConditions, ?rangeGetContentHash: bool) =
+            async {
+                let! token = Async.CancellationToken
 
-type BlobClient with
-    member this.AsyncDownload(?range: HttpRange, ?conditions: BlobRequestConditions, ?rangeGetContentHash: bool) =
-        async {
-            let! token = Async.CancellationToken
+                return!
+                    this.DownloadAsync(
+                        range = defaultArg range Unchecked.defaultof<_>,
+                        conditions = defaultArg conditions null,
+                        rangeGetContentHash = defaultArg rangeGetContentHash false,
+                        cancellationToken = token
+                    )
+                    |> Async.AwaitTask
+            }
 
-            return!
-                this.DownloadAsync(
-                    range = defaultArg range Unchecked.defaultof<_>,
-                    conditions = defaultArg conditions null,
-                    rangeGetContentHash = defaultArg rangeGetContentHash false,
-                    cancellationToken = token
-                )
-                |> Async.AwaitTask2
-        }
+        member this.AsyncExists() =
+            async {
+                let! token = Async.CancellationToken
 
-    member this.AsyncExists() =
-        async {
-            let! token = Async.CancellationToken
+                return!
+                    this.ExistsAsync(cancellationToken = token)
+                    |> Async.AwaitTask
+            }
 
-            return!
-                this.ExistsAsync(cancellationToken = token)
-                |> Async.AwaitTask2
-        }
+        member this.AsyncDelete(?snapshotsOption: DeleteSnapshotsOption, ?conditions: BlobRequestConditions) =
+            async {
+                let! token = Async.CancellationToken
 
-    member this.AsyncDelete(?snapshotsOption: DeleteSnapshotsOption, ?conditions: BlobRequestConditions) =
-        async {
-            let! token = Async.CancellationToken
+                return!
+                    this.DeleteAsync(
+                        snapshotsOption = defaultArg snapshotsOption Unchecked.defaultof<_>,
+                        conditions = defaultArg conditions null,
+                        cancellationToken = token
+                    )
+                    |> Async.AwaitTask
+            }
 
-            return!
-                this.DeleteAsync(
-                    snapshotsOption = defaultArg snapshotsOption Unchecked.defaultof<_>,
-                    conditions = defaultArg conditions null,
-                    cancellationToken = token
-                )
-                |> Async.AwaitTask2
-        }
+        member this.AsyncUpload
+            (
+                content: Stream,
+                ?httpHeaders: BlobHttpHeaders,
+                ?metadata: Collections.Generic.IDictionary<string, string>,
+                ?conditions: BlobRequestConditions,
+                ?progressHandler: IProgress<int64>,
+                ?accessTier: Nullable<AccessTier>,
+                ?transferOptions: Storage.StorageTransferOptions
+            ) =
+            async {
+                let! token = Async.CancellationToken
+
+                return!
+                    this.UploadAsync(
+                        content,
+                        httpHeaders = defaultArg httpHeaders null,
+                        metadata = defaultArg metadata null,
+                        conditions = defaultArg conditions null,
+                        progressHandler = defaultArg progressHandler null,
+                        accessTier = defaultArg accessTier (Nullable()),
+                        transferOptions = defaultArg transferOptions Unchecked.defaultof<_>,
+                        cancellationToken = token
+                    )
+                    |> Async.AwaitTask
+            }
+
+        member this.AsyncUpload
+            (
+                content: string,
+                ?httpHeaders: BlobHttpHeaders,
+                ?metadata: Collections.Generic.IDictionary<string, string>,
+                ?conditions: BlobRequestConditions,
+                ?progressHandler: IProgress<int64>,
+                ?accessTier: Nullable<AccessTier>,
+                ?transferOptions: Storage.StorageTransferOptions
+            ) =
+            async {
+                let! token = Async.CancellationToken
+
+                return!
+                    this.UploadAsync(
+                        content,
+                        httpHeaders = defaultArg httpHeaders null,
+                        metadata = defaultArg metadata null,
+                        conditions = defaultArg conditions null,
+                        progressHandler = defaultArg progressHandler null,
+                        accessTier = defaultArg accessTier (Nullable()),
+                        transferOptions = defaultArg transferOptions Unchecked.defaultof<_>,
+                        cancellationToken = token
+                    )
+                    |> Async.AwaitTask
+            }
+
+        member this.AsyncDeleteIfExists(?snapshotsOption: DeleteSnapshotsOption, ?conditions: BlobRequestConditions) =
+            async {
+                let! token = Async.CancellationToken
+
+                return!
+                    this.DeleteIfExistsAsync(
+                        snapshotsOption = defaultArg snapshotsOption Unchecked.defaultof<_>,
+                        conditions = defaultArg conditions null,
+                        cancellationToken = token
+                    )
+                    |> Async.AwaitTask
+            }
